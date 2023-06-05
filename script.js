2ee55cb9aa6581b904933c3c1784ac847e4db974
Dishank7 <127612933+Dishank7@users.noreply.github.com>
2023-06-05 17:44:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+let browserName = navigator.appName;
+
+let version = navigator.appVersion;
+
+
+let div = document.createElement("div");
+div.id = "browser-info";
+div.innerText = "You are using " + browserName + " version " + version
+
+let body = document.getElementsByTagName("body")[0];
+ body.append(div);
